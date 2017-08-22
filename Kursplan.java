60c020c3fa27cfb3e34dd326020c524e7f6fe941
JonasAndree <31105305+JonasAndree@users.noreply.github.com>
2017-08-22 08:58:04 +0200

    Kursplan

---
+++ Kursplan.java
@@ -0,0 +1,35 @@
+
+public class Kursplan {
+	public static void sentraltInnehåll() {
+		String objectOrientering1 = "Grunderna för klasserna, objekt, egenskaper och metoder.";
+		String objectOrientering2 = "Arv, inkapsling och polymorfism.";
+		String objectOrientering3 = "Skapande av klasser och objekt i ett objektorienterat programspråk utifrån "
+								  + "tidigare analys och design.";
+		String objectOrientering4 = "Användning av klasser och att genom arv förändra beteende hos klasser som ingår "
+								  + "i egna och andras klasshierarkier och standardbibliotek.";
+		String objectOrientering5 = "Generiska klasser och metoder.";
+		String objectOrientering6 = "Variablers och metoders synlighet och livslängd.";
+		String objectOrientering7 = "Stark och svag samt statisk och dynamisk typning.";
+		String objectOrientering8 = "Identifierares synlighet och livslängd.";
+		String objectOrientering9 = "Det valda programspråkets kontrollstrukturer.";
+		String objectOrientering10 = "Undantagshantering.";
+		String objectOrientering11 = "Analys, nedbrytning och modellering av programmeringstekniska problem med lämpligt "
+								   + "analysverktyg, till exempel användningsfall.";
+		String objectOrientering12 = "Design av lämplig lösning ur föregående analys med lämpligt verktyg och metoder "
+								   + "som klassdiagram.";
+		String objectOrientering13 = "Skapande av användarvänliga gränssnitt.";
+		String objectOrientering14 = "Skrivning och läsning av lagrad data.";
+		String objectOrientering15 = "Utveckling av program som nyttjar kommunikation över internet.";
+	}
+	public static void amnetsSyfte() {
+		String syfte1 = "Förmåga att formulera och planera programmeringsuppgifter med pseudokod och diagramteknik.";
+		String syfte2 = "Förståelse av och färdigheter i att använda datavetenskapliga begrepp och principer.";
+		String syfte3 = "Kunskaper om programspråk och programmeringsparadigm.\";
+		String syfte4 = "Förmåga att skriva, läsa, strukturera, analysera, dokumentera och kommentera programkod.";
+		String syfte5 = "Färdigheter i att skapa program med ett givet syfte och för en avsedd användare.";
+		String syfte6 = "Kunskaper om gränssnitt mot filer, filsystem, databashanterare och internet.";
+		String syfte7 = "Kunskaper om och färdigheter i objektorienterad programmering.";
+		String syfte8 = "Förmåga att finna, analysera, åtgärda och förhindra syntaxfel, programkörningsfel och programmeringslogiska fel.";
+		String syfte9 = "Kunskaper om samspelet mellan program, exekveringsmiljö, operativsystem och hårdvara.";
+	}
+}
