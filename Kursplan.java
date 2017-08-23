beec93bffa7a0d4a9e9d44e66736820dc36d7001
JonasAndree <31105305+JonasAndree@users.noreply.github.com>
2017-08-23 09:06:23 +0200

    Kursplan uppdaterad

--- Kursplan.java
+++ Kursplan.java
@@ -1,30 +1,59 @@
-
+/**
+ * Centrala innehållet i kursen
+ * 
+ * @author jonas.andree
+ * @version 0.6
+ * @category Programing 2 content
+ *
+ */
 public class Kursplan {
+	/**
+	 * Undervisningen i kursen ska behandla följande centrala innehåll.
+	 */
 	public static void sentraltInnehåll() {
-		String objectOrientering1 = "Grunderna för klasserna, objekt, egenskaper och metoder.";
-		String objectOrientering2 = "Arv, inkapsling och polymorfism.";
-		String objectOrientering3 = "Skapande av klasser och objekt i ett objektorienterat programspråk utifrån "
+		String sentraltInnehåll1 = "Grunderna för klasserna, objekt, egenskaper och metoder.";
+		
+		String sentraltInnehåll2 = "Arv, inkapsling och polymorfism.";
+		
+		String sentraltInnehåll3 = "Skapande av klasser och objekt i ett objektorienterat programspråk utifrån "
 								  + "tidigare analys och design.";
-		String objectOrientering4 = "Användning av klasser och att genom arv förändra beteende hos klasser som ingår "
+		
+		String sentraltInnehåll4 = "Användning av klasser och att genom arv förändra beteende hos klasser som ingår "
 								  + "i egna och andras klasshierarkier och standardbibliotek.";
-		String objectOrientering5 = "Generiska klasser och metoder.";
-		String objectOrientering6 = "Variablers och metoders synlighet och livslängd.";
-		String objectOrientering7 = "Stark och svag samt statisk och dynamisk typning.";
-		String objectOrientering8 = "Identifierares synlighet och livslängd.";
-		String objectOrientering9 = "Det valda programspråkets kontrollstrukturer.";
-		String objectOrientering10 = "Undantagshantering.";
-		String objectOrientering11 = "Analys, nedbrytning och modellering av programmeringstekniska problem med lämpligt "
+		
+		String sentraltInnehåll5 = "Generiska klasser och metoder.";
+		
+		String sentraltInnehåll6 = "Variablers och metoders synlighet och livslängd.";
+		
+		String sentraltInnehåll7 = "Stark och svag samt statisk och dynamisk typning.";
+		
+		String sentraltInnehåll8 = "Identifierares synlighet och livslängd.";
+		
+		String sentraltInnehåll9 = "Det valda programspråkets kontrollstrukturer.";
+		
+		String sentraltInnehåll10 = "Undantagshantering.";
+		
+		String sentraltInnehåll11 = "Analys, nedbrytning och modellering av programmeringstekniska problem med lämpligt "
 								   + "analysverktyg, till exempel användningsfall.";
-		String objectOrientering12 = "Design av lämplig lösning ur föregående analys med lämpligt verktyg och metoder "
+		
+		String sentraltInnehåll12 = "Design av lämplig lösning ur föregående analys med lämpligt verktyg och metoder "
 								   + "som klassdiagram.";
-		String objectOrientering13 = "Skapande av användarvänliga gränssnitt.";
-		String objectOrientering14 = "Skrivning och läsning av lagrad data.";
-		String objectOrientering15 = "Utveckling av program som nyttjar kommunikation över internet.";
+		
+		String sentraltInnehåll13 = "Skapande av användarvänliga gränssnitt.";
+		
+		String sentraltInnehåll14 = "Skrivning och läsning av lagrad data.";
+		
+		String sentraltInnehåll15 = "Utveckling av program som nyttjar kommunikation över internet.";
 	}
+	/**
+	 * Undervisningen i ämnet programmering ska ge eleverna förutsättningar att utveckla följande.
+	 */
 	public static void amnetsSyfte() {
+		
 		String syfte1 = "Förmåga att formulera och planera programmeringsuppgifter med pseudokod och diagramteknik.";
+		
 		String syfte2 = "Förståelse av och färdigheter i att använda datavetenskapliga begrepp och principer.";
-		String syfte3 = "Kunskaper om programspråk och programmeringsparadigm.\";
+		String syfte3 = "Kunskaper om programspråk och programmeringsparadigm.";
 		String syfte4 = "Förmåga att skriva, läsa, strukturera, analysera, dokumentera och kommentera programkod.";
 		String syfte5 = "Färdigheter i att skapa program med ett givet syfte och för en avsedd användare.";
 		String syfte6 = "Kunskaper om gränssnitt mot filer, filsystem, databashanterare och internet.";
