7475bbbe2a50811327e688d5a0aeeb5366696755
JonasAndree <31105305+JonasAndree@users.noreply.github.com>
2017-09-01 09:23:41 +0200

    Lesson Arrays Exercises

---
+++ src/lesson/arrays/exercises/ArrayExercises.java
@@ -0,0 +1,51 @@
+package lesson.arrays.exercises;
+/**
+ * 
+ * @author jonas.andree
+ * @category Exercises in arrays 
+ * @version 1.0
+ */
+public class ArrayExercises {
+	public static void main(String[] args) {
+		
+		System.out.println("You can finde help at https://docs.oracle.com/javase/7/docs/api/java/util/Arrays.html \n "
+				+ "and always make sure you print out the result to chek if it is correct.\n");
+		
+		System.out.println("1. Write a Java program to sort an numeric array and a string array.\nUse:\n"
+				+ "int[] my_array1 = { 1789, 2035, 1899, 1456, 2013, 1458, 2458, 1254, 1472, 2365, 1456, 2165, 1457, 2456 };\n"
+				+ "String[] my_array2 = { \"Java\", \"Python\", \"PHP\", \"C#\", \"C Programming\", \"C++\" };\n");
+
+		System.out.println("2. Write a Java program to sum values of an array.\nUse:\n"
+				+ "int[] my_array1 = { 1789, 2035, 1899, 1456, 2013, 1458, 2458, 1254, 1472, 2365, 1456, 2165, 1457, 2456 };\n");
+		
+		System.out.println("3. Write a Java program to print the following grid. Using a two dimentional array\n"
+		+ "Expected Output :\n"
+		+"	- - - - - - - - - - \n"                                                                                         
+		+"	- - - - - - - - - - \n"                                                                                         
+		+"	- - - - - - - - - - \n"                                                                                         
+		+"	- - - - - - - - - - \n"                                                                                         
+		+"	- - - - - - - - - - \n"                                                                                         
+		+"	- - - - - - - - - - \n"                                                                                         
+		+"	- - - - - - - - - - \n"                                                                                         
+		+"	- - - - - - - - - - \n"                                                                                         
+		+"	- - - - - - - - - - \n"                                                                                         
+		+"	- - - - - - - - - - \n"
+		+"");
+		
+		System.out.println("4. Write a Java program to calculate the average value of array elements.\nUse:\n" 
+				+ "int[] my_array1 = { 1789, 2035, 1899, 1456, 2013, 1458, 2458, 1254, 1472, 2365, 1456, 2165, 1457, 2456 };\n");
+		
+		System.out.println("5. Write a Java program to test if an array contains a specific value.\n Use same array as yes in the previus exercise.\n And print the result.\n");
+		System.out.println("6.Write a Java program to find the index of an array element.\n And print the result.\n");
+		System.out.println("7. Write a Java program to remove a specific element from an array.\n And print the result.+\n");
+		System.out.println("8. Write a Java program to copy an array by iterating the array. \n And print the result.\n");
+		System.out.println("9. Write a Java program to insert an element (specific position) into an array. \\n");
+		System.out.println("10. Write a Java program to find the maximum and minimum value of an array.\n");
+		System.out.println("11. Write a Java program to reverse an array of integer values.\n");
+		System.out.println("12. Write a Java program to find the duplicate values of an array of integer values.\n");
+		System.out.println("13. Write a Java program to find the duplicate values of an array of string values.\n");
+		System.out.println("14. Write a Java program to remove duplicate elements from an array. \n");
+		System.out.println("15. Write a Java program to find the second largest element in an array.\n");
+		System.out.println("16. Write a Java program to find the second smallest element in an array. \n");
+	}
+}
