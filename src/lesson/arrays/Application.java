376e102f80745f22867808ad70b694174c384a99
JonasAndree <31105305+JonasAndree@users.noreply.github.com>
2017-09-04 13:43:41 +0200

    Exercis Arrays 1

---
+++ src/lesson/arrays/Application.java
@@ -0,0 +1,18 @@
+package lesson.arrays;
+
+import java.util.Arrays;
+
+public class Application {
+
+	public static void main(String[] args) {
+		int[] my_array1 = { 1789, 2035, 1899, 1456, 2013, 1458, 2458, 1254, 1472, 2365, 1456, 2165, 1457, 2456 };
+		String[] my_array2 = { "Java", "Python", "PHP", "C#", "C Programming", "C++" };
+		
+		Arrays.sort(my_array1);
+		Arrays.sort(my_array2);
+		
+		System.out.println(Arrays.toString(my_array1));
+		System.out.println(Arrays.toString(my_array2));
+	}
+
+}
