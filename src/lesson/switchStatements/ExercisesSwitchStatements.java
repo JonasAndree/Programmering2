1765d2daca0723f4ea3feaa766d564dde03b8fe4
JonasAndree <31105305+JonasAndree@users.noreply.github.com>
2017-10-11 09:50:10 +0200

    Lesson Switch statements

---
+++ src/lesson/switchStatements/ExercisesSwitchStatements.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package lesson.switchStatements;
+
+/**
+ * http://faculty.salina.k-state.edu/tmertz/Java/042controlstatements/06thejavaswitchstatement.pdf
+ * 
+ * Exercise 1: Write a Java program that allows the user to choose the correct answer of a question.
+ * 
+ * See the example below:
+ * 	What is the correct way to declare a variable to store an integer value in Java?
+ * 		a. int 1x=10;
+ * 		b. int x=10;
+ * 		c. float x=10.0f;
+ * 		d. string x="10";
+ * 		Enter your choice: c
+ * 
+ * @author jonas.andree
+ *
+ */
+public class ExercisesSwitchStatements {
+
+}
