93b651a8fd8a94b6bab2f710dcc743f78a4cd732
JonasAndree <31105305+JonasAndree@users.noreply.github.com>
2017-10-16 11:29:33 +0200

    min

--- src/lesson/switchs/MainMaze.java
+++ src/lesson/switchs/MainMaze.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class MainMaze {
 	
 	private final static String MAZE =
-			"#######################\n" +
+			"#############G#########\n" +
 			"# #       # #         #\n" +
 			"# # # ### # # ####### #\n" +
 			"#   #   #   # #   #   #\n" +
@@ -16,18 +16,24 @@ public class MainMaze {
 			"# # ### # # # ####### #\n" +
 			"# # #     # #   #     #\n" +
 			"# # ########### ### # #\n" +
-			"  # #         #     # #\n" +
+			"S # #         #     # #\n" +
 			"### # ####### ####### #\n" +
 			"#   # #     #         #\n" +
 			"# ### ##### ###########\n" +
 			"#                     #\n" +
 			"#######################\n";
+	private static int startX;
+	private static int startY;
+	
+	private static int endX;
+	private static int endY;
 	
 	private static ArrayList<ArrayList<Character>> arrayMaze;
 
 	public static void main(String[] args) {
 		System.out.println(MAZE);
 		parsMaze();
+		drawMaze();
 	}
 	
 	public static void parsMaze() {
@@ -37,18 +43,64 @@ public class MainMaze {
 		arrayMaze = new ArrayList<>();
 		for (char cell : MAZE.toCharArray() ) {
 			switch (cell) {
-			case '#':
-				row.add(cell);
-				x++;
-			case ' ':
-				row.add(cell);
-				x++;
-			case '\n':
-				arrayMaze.add(row);
-				row = new ArrayList<>();
-				y++;
-				x = 0;
+			case '#':  row.add(cell);
+					   x++;
+					   break;
+			case ' ':  row.add(cell);
+					   x++;
+					   break;
+			case 'S':  row.add(' ');
+					   startX = x;
+					   startY = y;
+					   x++;
+					   break;
+			case 'G':  row.add(' ');
+					   endX = x;
+					   endY = y;
+					   x++;
+					   break;
+			case '\n': arrayMaze.add(row);
+					   row = new ArrayList<>();
+					   y++;
+					   x = 0;
+					   break;
 			}
 		}
 	}
+	
+	public static void drawMaze() {
+		for(ArrayList<Character> row : arrayMaze) {
+			for(char cell : row) {
+				if (cell == '#') {
+					cell = '\u2588';
+				}
+				System.out.print(cell);
+				System.out.print(cell);
+			}
+			System.out.println();
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
